6f051a143bb5d5217d89ed99418cf1490331ed19
evolvestin <evolvestin@gmail.com>
2022-10-05 19:31:01 +0300

    test

--- main.py
+++ main.py
@@ -1,7 +1,20 @@
+import _thread
+from time import sleep
 from flask import Flask
 app = Flask(__name__)
 
 
+def herl():
+    count = 0
+    while True:
+        count += 1
+        print('привет', count)
+        sleep(5)
+
+
 @app.route('/')
 def hello_world():
+    _thread.start_new_thread(herl, ())
     return 'Hello, World!'
+
+# app.run(host='0.0.0.0', port='5000', debug=True)
