8b6063917ccc49ced38278b912c070fc24a95c8f
evolvestin <evolvestin@gmail.com>
2022-10-05 19:47:44 +0300

    test

--- main.py
+++ main.py
@@ -1,13 +1,17 @@
 import _thread
+import os
 from time import sleep
 from flask import Flask
 app = Flask(__name__)
 
 
 def herl():
+    from telebot import TeleBot
     count = 0
+    bot = TeleBot(os.environ['go'])
     while True:
         count += 1
+        bot.send_message(396978030, f'привет {count}')
         print('привет', count)
         sleep(5)
 
@@ -16,5 +20,13 @@ def herl():
 def hello_world():
     _thread.start_new_thread(herl, ())
     return 'Hello, World!'
+
+
+def main():
+    app.run(host='0.0.0.0', port=5000)
+
+
+if __name__ == '__main__':
+    main()
 
 # app.run(host='0.0.0.0', port='5000', debug=True)
